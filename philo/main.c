7daad1e5b115868af42d24c9d86fabe8c35da964
Inheert <theoclaereboudt@gmail.com>
2024-10-13 12:00:49 +0200

    todo

--- philo/main.c
+++ philo/main.c
@@ -6,11 +6,15 @@
 /*   By: Théo <theoclaereboudt@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 09:50:04 by Théo              #+#    #+#             */
-/*   Updated: 2024/10/13 10:12:09 by Théo             ###   ########.fr       */
+/*   Updated: 2024/10/13 10:57:02 by Théo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+// Manage args
+// Create structure to store philo and philo params
+// ++++++++++++++.+.+.+.+.+.+.+.+
 
 int	main(int argc, char **argv)
 {
